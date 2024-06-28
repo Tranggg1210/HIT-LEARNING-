882b2f145df0342c0052bdf8091b19832f5df500
tungvhn <tungviet624@gmail.com>
2024-06-28 18:07:14 +0700

    🐛 fix responsive mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Route, Router, Routes, useRoutes } from 'react-router-dom'
 import './App.scss'
 import Home from './pages/Home/Home'
-// import Login from './pages/Login/Login'
+import Login from './pages/Login/Login'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />}></Route>
-        {/* <Route path='/signIn' element={<Login />}></Route> */}
+        <Route path='/signIn' element={<Login />}></Route>
       </Routes>
     </>
   )
--- src/components/Classes/Classes.jsx
+++ src/components/Classes/Classes.jsx
@@ -3,29 +3,10 @@ import './Classes.scss'
 
 const Classes = () => {
   const access_token = localStorage.getItem('access_token')
-  const classs = [
-    { title: 'TV Series', api: 'phim-bo' },
-    // { title: 'Movies', api: 'phim-le' },
-  ]
-  // const c = [
-  //   { title: 'TV Series', api: 'phim-bo' },
-  //   { title: 'Movies', api: 'phim-le' },
-  // ]
+  const classs = [{ title: 'TV Series', api: 'phim-bo' }]
+
   return (
     <>
-      {/* <div>
-        {access_token
-          ? c.map((claases) => (
-              <div key={claases.title} className='category-container'>
-                <ClassesItem className='relative' title={claases.title} api={claases.api} />
-              </div>
-            ))
-          : classs.map((claass) => (
-              <div key={claass.title} className='category-container'>
-                <ClassesItem className='relative' title={claass.title} api={claass.api} />
-              </div>
-            ))}
-      </div> */}
       {classs.map((claass) => (
         <div key={claass.title} className='category-container'>
           <ClassesItem className='relative' title={claass.title} api={claass.api} />
