88bcaafcf642455ed2883c393b7d01bec086ff8c
tungvhn <tungviet624@gmail.com>
2024-07-04 11:32:28 +0700

    🐛 fixx config

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,13 @@
 import Slider from '../../components/Slider/Slider'
 import Classes from '../../components/Classes/Classes'
 import './Home.scss'
-import FooterHL from '../../common/FooterHL/FooterHL'
 import Classess from '../../components/Classes2/Classess'
-import CourseLeader from '../../components/CourseLeader/CourseLeader'
-import CourseList from '../../components/CourseList/CourseList'
-import CreateFolder from '../../components/CreateFolder/CreateFolder'
-import CreateSubFolder from '../../components/CreateSubFolder/CreateSubFolder'
+
 const Home = () => {
   const access_token = localStorage.getItem('access_token')
   return (
     <>
+      zx
       <div className='content'>
         <Slider />
         {access_token ? <Classess /> : <Classes />}
