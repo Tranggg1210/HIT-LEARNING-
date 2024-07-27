c06b1e895d5d9cd0b254d41556e5d9f826a39b81
khuong09072004 <148343430+khuong09072004@users.noreply.github.com>
2024-07-27 23:02:05 +0700

    fix-api-basicoures

--- src/apis/item.api.js
+++ src/apis/item.api.js
@@ -7,4 +7,4 @@ const itemsApi = () => ({
   deleteItem: async (id) => apiDefault.delete(`${ApiConstant.items.deleteItem}${id}`),
   getItemBySectionId: async (id) => apiDefault.get(`${ApiConstant.items.getItemBySectionId}${id}`),
 })
-export const { getAllItem, createItem, deleteItem,getItemBySectionId } = coursesApi()
+export const { getAllItem, createItem, deleteItem,getItemBySectionId } =  itemsApi()
--- src/pages/BasicCourse/BasicCourse.jsx
+++ src/pages/BasicCourse/BasicCourse.jsx
@@ -7,6 +7,7 @@ import { useNavigate, useParams } from 'react-router-dom'
 import MainLayout from '../../layouts/Layout/MainLayout'
 import { getCourseById } from '../../apis/courses.api'
 import { getSectionByCourseId } from '../../apis/section.api'
+import { getItemBySectionId } from '../../apis/item.api'
 
 
 const BasicCourse = () => {
@@ -21,77 +22,67 @@ const BasicCourse = () => {
   const handleCourse = () => {
     navigate('/courseLeader')
   }
-  const handleToggle = (index) => {
-    setOpenSection(openSection === index ? null : index)
-  }
-
-
-
   const param = useParams();
-
-
   const loadDataCourses = async () => {
     try {
       const response = await getCourseById(param.id);
       (response && response.data && response.data.data)
       const result = response.data.data;
-      setBasicCourses([result]);
+      setBasicCourses(result);
     } catch (error) {
-      alert(error.response?.data?.message);
+      console.log(error.response?.data?.message);
     }
   };
-
-
   const loadDataSections = async () => {
     try {
-      const response1 = await getSectionByCourseId(param.id);
-      if (response1 && response1.data && response1.data.data) {
-        setSections(response1.data.data);
+      const response = await getSectionByCourseId(param.id);
+      if (response && response.data && response.data.data) {
+        setSections(response.data.data.content);
+       
       }
     } catch (error) {
-      alert(error.response1?.data?.message);
+      console.log(error.response?.data?.message);
     }
   };
-  // const loadDataItem =async () =>{
-  //   try{
-  //     const response2 = await getSectionByCourseId(param.id);
-  //     (response2 && response2.data && response2.data.data)
-  //     const result2= response2.data.data;
-  //     setItems(result2);
-  //   }catch(error){
-  //     alert(error.response2?.data?.message);
-  //   }
-  // };
-
-
-
   useEffect(() => {
     if (param.id) {
       loadDataCourses();
       loadDataSections();
+      
     }
   }, [param.id]);
-  console.log("Course ID:", param.id);
-  console.log("Sections: ", sections);
-  // console.log("Items: ", items);
+  // console.log("Sections: ", sections);
+  
+  const loadDataItem =async (id) =>{
+    try{
+      const response = await getItemBySectionId(id);
+      (response && response.data && response.data.data)
+      const result= response.data.data.content;
+      setItems(result);
+    }catch(error){
+      console.log(error.response?.data?.message);
+    }
+  };
 
-
-
+  const handleToggle = (index,id) => {
+    setOpenSection(openSection === index ? null : index)
+    loadDataItem(id);
+    
+  }
   return (
     <>
 
       <div className='course-page'>
         <div className='course-basic-left'>
-          {Array.isArray(basicCourses) && basicCourses.map((i, index) => (
-            <div key={index} className='course-header'>
-              <h1>{i.name}</h1>
+          
+            <div className='course-header'>
+              <h1>{basicCourses?.name}</h1>
               <p className='describe'>
-                {i.description}
+                {basicCourses?.description}
               </p>
             </div>
 
-          ))}
-
+          
           <div className='course-content'>
             <div className='lesson-content'>
               <h2>Nội dung khóa học</h2>
@@ -108,15 +99,15 @@ const BasicCourse = () => {
                   </div>
                   {openSection === index && (
                     <div className='section-content'>
-                      {/* {items.length > 0 ? (
-                      items.map((item, idx) => (
-                        <div key={idx} className='item'>
+                      {items.length > 0 ? (
+                      items.map((item) => (
+                        <div key={item.id} className='item'>
                           <span>{item.name}</span>
                         </div>
                       ))
                     ) : (
                       <div className='no-lesson'>Không có bài học</div>
-                    )} */}
+                    )}
                     </div>
                   )}
                 </div>
@@ -124,17 +115,15 @@ const BasicCourse = () => {
             </div>
           </div>
         </div>
-        {Array.isArray(basicCourses) && basicCourses.map((i, index) => (
-          <div className='course-basic-right' key={index}>
+        
+          <div className='course-basic-right'>
             <div className='course-video'>
-              <img src={`https://hitproduct2024-production-a244.up.railway.app/stream/${i.videoId}`} alt='Khóa học' />
+              <img src={`https://hitproduct2024-production-a244.up.railway.app/stream/${basicCourses.videoId}`} alt='Khóa học' />
               <Button variant='contained' color='primary' onClick={handleCourse}>
                 Xem tài liệu
               </Button>
             </div>
           </div>
-        ))}
-
       </div>
     </>
   )
