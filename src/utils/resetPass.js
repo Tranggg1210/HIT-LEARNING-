1f0381a2d6f290a7d3c4f5dcb926f9e110d2e430
vthn303 <vthn303@gmail.com>
2024-08-08 19:43:39 +0700

    fix: index.scss

--- src/utils/resetPass.js
+++ src/utils/resetPass.js
@@ -5,8 +5,8 @@ export const resetPass = () =>
     newPass: Yup.string()
       .required('Mật khẩu là bắt buộc')
       .matches(
-        /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/,
-        'Mật khẩu có ít nhất 8 ký tự bao gồm ít nhất 1 ký tự viết hoa, ít nhất 1 số',
+        /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/,
+        'Mật khẩu có ít nhất 6 ký tự bao gồm ít nhất 1 ký tự viết hoa, ít nhất 1 số',
       ),
     confirmPass: Yup.string()
       .required('Xác nhận mật khẩu mới là bắt buộc')
